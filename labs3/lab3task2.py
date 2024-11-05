9f1007ac016d6fcdc7f857ed7273f4c1a8999540
Mifegio <countycheshire@gmail.com>
2024-11-06 00:03:35 +0300

    Update lab3task2.py

--- labs3/lab3task2.py
+++ labs3/lab3task2.py
@@ -2,8 +2,9 @@
 def find_common_participants(first, second, separator=','):
     participants1 = set(first.split(separator))
     participants2 = set(second.split(separator))
-    participants = participants1.intersection(participants2)
-    return list(participants)
+    participants = list(participants1.intersection(participants2))
+    participants.sort()
+    return participants
 
 
 participants_first_group = "Иванов|Петров|Сидоров"
@@ -11,4 +12,4 @@ participants_second_group = "Петров|Сидоров|Смирнов"
 
 # TODO Провеьте работу функции с разделителем отличным от запятой
 participants = find_common_participants(participants_first_group, participants_second_group, '|')
-print("Количество общих участников:", sorted(participants))
+print("Количество общих участников:", participants)
